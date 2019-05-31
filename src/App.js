59321712c9b5acad88dcd1aa58e63de303823bb2
sukhber.bansal <sukhber.bansal@iongroup.com>
2019-05-31 19:08:13 +0530

    flexible lists

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import Person from './Person/Person';
 
 class App extends Component {
 
-     state = 
-     {
-       persons : 
-      [
-        {name:"modi", age:65},
-        {name:"sunny", age:55},
-        {name:"smriti", age:45}
-      ],
-      otherState: 'bhajpa'
-     }
+     
 
      switchNameHandler = (newName1, newName2) => {
        //console.log('Was Clicked');
@@ -27,16 +18,7 @@ class App extends Component {
        } );
      }
 
-     nameChangedHandler = (event) => {
-      this.setState( {
-        persons : 
-        [
-          {name: 'NaMo', age:65},
-          {name: event.target.value, age:55},
-          {name:"Irani", age:45}
-        ]  
-       } );
-     }
+     
      /* 
      const [personsState, setpersonsState] = useState({
       persons : 
@@ -65,6 +47,44 @@ class App extends Component {
     console.log(personsState);
     console.log(otherState);
 */
+togglePersonsHandler = () => {
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons: !doesShow});
+}
+
+deletePersonHandler = (personIndex) => {
+  //const persons = this.state.persons.splice();    another way of creating a copy
+const persons = [...this.state.persons];   //creating a copy named persons using spread operator from original persons
+persons.splice(personIndex, 1); //removes 1 element from the given index
+this.setState({persons: persons});
+}
+
+nameChangedHandler = (event, id) => {
+  const personIndex = this.state.persons.findIndex(p => {
+    return p.id === id;
+  })
+  const person = {
+    ...this.state.persons[personIndex]
+  }
+  //const person = Object.assign({}, this.state.persons[personIndex]);    other way of creating person
+  person.name = event.target.value;
+  const persons = [...this.state.persons];
+  persons[personIndex] = person;
+ this.setState( {persons: persons} );
+}
+
+state = 
+     {
+       persons : 
+      [
+        {id:"hgfhf", name:"modi", age:65},
+        {id:"hgchg", name:"sunny", age:55},
+        {id:"jhryd", name:"smriti", age:45}
+      ],
+      otherState: 'bhajpa',
+      showPersons: false
+     }
+
 render() {
   const style = {
     backgroundColor: 'white',
@@ -74,15 +94,33 @@ render() {
     cursor: 'pointer'
   };
 
+  let persons = null;
+
+  if(this.state.showPersons) {
+    persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+          return <Person 
+          click={this.deletePersonHandler.bind(this, index)} 
+          name={person.name} 
+          age={person.age} 
+          key={person.id} 
+          changed={(event) => this.nameChangedHandler(event, person.id)}>Balwant Rai Ke Kutte</Person>
+        })}
+        </div>
+    );
+  }
+
   return (
     <div className="App">
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <button style={style} onClick={this.switchNameHandler.bind(this, 'NaMo', 'Deol')}>Switch Name</button>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'NaMo', 'Sunny Paaji')} changed={this.nameChangedHandler} />
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      <button style={style} onClick={this.togglePersonsHandler}>Switch Persons</button>
+        {persons}
     </div>
   );
 }  
 }
-
 export default App;
+/*
+<Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'NaMo', 'Sunny Paaji')} changed={this.nameChangedHandler} />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />*/
